4bbbc17e8170528022ec737c7b6a3463c5064fb0
Raul Pires Ferreira Leal <raulpfl@hotmail.com>
2020-07-06 19:59:23 -0300

    Initial commit

---
+++ raul/src/test/java/com/example/raul/RaulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
